d03717de0d15cf29d59cdbafe313e2c2e5e6185b
Henry Paulino <henrypl360@gmail.com>
2020-06-29 23:16:01 -0400

    Initial Commit

---
+++ MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by Henry Paulino on 6/29/20.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
